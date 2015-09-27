da719db3469863c7ba7f95d84d69a3836328f2e8
Sam Coope <sam.j.coope@gmail.com>
2015-09-27 15:06:17 +0100

    changed over to express

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 var http = require('http');
 var dispatch = require('dispatch');
 var pageBuilder = require('./pageBuilder');
+var express = require('express');
+
+var server = express();
 
 var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8087;
-console.log(process);
+var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var dispatcher = dispatch({
     '/': function(request, response) {
@@ -30,24 +33,47 @@ var dispatcher = dispatch({
     }
 });
 
+server.get('/', function(request, response) {
+    pageBuilder.buildIndex()
+    .then(function(page) {
+        deliverPage(response, page);
+    });
+});
+server.get('/posts/:post', function(request, response) {
+    pageBuilder.buildPost(request.params.post)
+    .then(function(page) {
+        deliverPage(response, page);
+    });
+});
+server.get('/about', function(request, response) {
+    pageBuilder.buildAboutPage()
+    .then(function(page) {
+        deliverPage(response, page);
+    });
+});
+server.get('/:other', function(request, response) {
+    response.writeHead(404);
+    response.end('404: There\'s nothing here');
+});
+
 function deliverPage(response, page) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(page);
     response.end();
 }
 
-var server = http.createServer(function(request, response) {
-    try {
-        dispatcher(request, response);
-    } catch (err) {
-        console.error(err);
-        response.writeHead(500);
-        response.end('Internal Server Error');
-    }
-});
+// var server = http.createServer(function(request, response) {
+//     try {
+//         dispatcher(request, response);
+//     } catch (err) {
+//         console.error(err);
+//         response.writeHead(500);
+//         response.end('Internal Server Error');
+//     }
+// });
 
 pageBuilder.initialise().then(function() {
-    server.listen(PORT, function() {
-        console.log('Server listening on: http://localhost:%s', PORT);
+    server.listen(PORT, IPADDRESS, function() {
+        console.log('Server listening on port:%s', PORT);
     });
 });
